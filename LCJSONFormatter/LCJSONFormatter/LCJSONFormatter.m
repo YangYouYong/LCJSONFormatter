4f72cfaf2d9eb1bfa2d7ca3f67854ce8b8cddc93
Apple <apple@AppledeMacBook-Pro-2.local>
2017-04-28 12:44:19 +0800

    update Argo to JSONAbleType

--- LCJSONFormatter/LCJSONFormatter/LCJSONFormatter.m
+++ LCJSONFormatter/LCJSONFormatter/LCJSONFormatter.m
@@ -17,6 +17,7 @@ static LCJSONFormatter *sharedPlugin;
 @property (nonatomic, strong) NSTextView *currentTextView;
 @property (nonatomic, copy) NSString *currentFilePath;
 @property (nonatomic) BOOL swift;
+@property (nonatomic, strong) NSString *inFunctionStr;
 @property (nonatomic, strong) NSString *propertyContentStr;
 @property (nonatomic, strong) NSString *detailContentStr;
 @property (nonatomic, strong) NSString *totalFuncStr;
@@ -131,6 +132,7 @@ static LCJSONFormatter *sharedPlugin;
     
     self.detailContentStr = @"";
     self.propertyContentStr = @"";
+//    self.inFunctionStr = @"init(";
     self.firstParseDetailContent = YES;
     
     for (NSString *key in structInfo.structDic) {
@@ -144,27 +146,67 @@ static LCJSONFormatter *sharedPlugin;
 
             if ([obj isKindOfClass:[NSDictionary class]]) {
                 NSString *formatStr = [self formatDicWithKey:key value:childClassName];
-                self.propertyContentStr = [NSString stringWithFormat:@"%@\n%@",self.propertyContentStr ,formatStr];
+                if(self.propertyContentStr.length <= 0){
+                    self.propertyContentStr = formatStr;
+                }else{
+                    self.propertyContentStr = [NSString stringWithFormat:@"%@\n%@",self.propertyContentStr ,formatStr];
+                }
                 
-                NSString *detailStr = [self formatDetailWithKey:key next:1];
+//                NSString *detailStr = [self formatDetailWithKey:key next:1];
+                NSString *detailStr = [self formatDetailWithKey:key childClass:childClassName next:1 context:structInfo];
                 self.detailContentStr = [NSString stringWithFormat:@"%@%@",self.detailContentStr ,detailStr];
             } else {
                 NSString *formatStr = [self formatArrayWithKey:key value:childClassName];
                 self.propertyContentStr = [NSString stringWithFormat:@"%@\n%@",self.propertyContentStr ,formatStr];
                 
                 
-                NSString *detailStr = [self formatDetailWithKey:key next:2];
+                NSString *detailStr = [self formatDetailWithKey:key childClass:childClassName next:2 context:structInfo];
+//                [self formatDetailWithKey:key next:2];
                 self.detailContentStr = [NSString stringWithFormat:@"%@%@",self.detailContentStr ,detailStr];
             }
+//            self.inFunctionStr = [NSString stringWithFormat:@"%@%@",self.inFunctionStr,]
             
         } else {
             NSString *formatStr = [self formatPropertyWithKey:key value:obj];
             self.propertyContentStr = [NSString stringWithFormat:@"%@\n%@",self.propertyContentStr ,formatStr];
             
-            NSString *detailStr = [self formatDetailWithKey:key next:0];
+            NSString *detailStr = [self formatDetailWithKey:key childClass:obj next:0 context:structInfo];
+//            [self formatDetailWithKey:key next:0];
             self.detailContentStr = [NSString stringWithFormat:@"%@%@",self.detailContentStr ,detailStr];
         }
     }
+    
+    NSString *initPropertiesList = [self.propertyContentStr stringByReplacingOccurrencesOfString:@"var" withString:@""];
+    initPropertiesList = [initPropertiesList stringByReplacingOccurrencesOfString:@"\n" withString:@","];
+    initPropertiesList = [initPropertiesList stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    
+   __block NSString *newInitPropertiesList = @"";
+    
+    NSArray *properties = [self.propertyContentStr componentsSeparatedByString:@"\n"];
+    NSMutableArray *settedProperties = @[].mutableCopy;
+    NSMutableArray *setReturnPrperties = @[].mutableCopy;
+    [properties enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"plugin_properties_%@",obj);
+        obj = [obj stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        if ([newInitPropertiesList isEqualToString:@""]) {
+            newInitPropertiesList = [obj stringByReplacingOccurrencesOfString:@"var " withString:@""];
+        }else{
+            newInitPropertiesList = [newInitPropertiesList stringByAppendingString:[obj stringByReplacingOccurrencesOfString:@"var " withString:@", "]];
+        }
+        
+        NSInteger sepIndex = [obj rangeOfString:@":"].location;
+        if (obj.length > sepIndex) {
+            NSString *propertyName = [obj substringToIndex:sepIndex];
+            propertyName = [propertyName stringByReplacingOccurrencesOfString:@"var" withString:@""];
+            propertyName = [propertyName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            NSString *newObj = [NSString stringWithFormat:@"\n      \tself.%@ = %@", propertyName, propertyName];
+            [settedProperties addObject:newObj];
+            NSString *newProper = [NSString stringWithFormat:@"%@: %@", propertyName,propertyName];
+            [setReturnPrperties addObject:newProper];
+        }
+    }];
+    
+    self.inFunctionStr = [NSString stringWithFormat:@"\n    init(%@) {\n %@ \n    }",newInitPropertiesList, [settedProperties componentsJoinedByString:@"\t"]];
     
     
     NSString *structName = nil;
@@ -174,7 +216,14 @@ static LCJSONFormatter *sharedPlugin;
         structName = structInfo.structName;
     }
     
-    NSString *totalFuncStr = [NSString stringWithFormat:headerFuncStr, structName, self.propertyContentStr,structName, self.detailContentStr];
+//    NSString *totalFuncStr = [NSString stringWithFormat:headerFuncStr, structName, self.propertyContentStr,structName, self.detailContentStr];
+    
+    // className  properties initfunc  decodefunc  returnObject
+    
+//    @"\n final class %@: NSObject, JSONAbleType { \n %@ \n %@\n    static func fromJSON(_ json: [String : AnyObject]) -> %@ {\n          let json = JSON(json)%@\n %@    }\n}\n "
+    NSString *returnObjStr = [NSString stringWithFormat:@"\n    \treturn %@(%@)", structName,[setReturnPrperties componentsJoinedByString:@", "]];
+    
+    NSString *totalFuncStr = [NSString stringWithFormat:decodeFuncStr, structName, self.propertyContentStr,self.inFunctionStr,structName, self.detailContentStr,returnObjStr];
     self.totalFuncStr = [NSString stringWithFormat:@"%@\n%@",self.totalFuncStr,totalFuncStr];
     
     if ([structInfo.infoDic allKeys].count != 0) {
@@ -190,48 +239,104 @@ static LCJSONFormatter *sharedPlugin;
 
 #pragma mark -
 #pragma mark convert to Argo
+#pragma mark convert to JSONAbleType
+
 //nextFlag ==0 property;== 1 dic; == 2 array
-- (NSString *)formatDetailWithKey:(NSString *)key next:(NSInteger)nextFlag{
-    NSString *indexStr = @"<*>";
-    if (self.firstParseDetailContent == YES) {
-        indexStr = @"<^>";
-        self.firstParseDetailContent = NO;
+
+- (NSString *)formatDetailWithKey:(NSString *)key
+                       childClass:(NSObject *)childClassName
+                             next:(NSInteger)nextFlag
+                          context:(LCStructInfo *)structInfo {
+    
+    NSString *typeStr = (NSString *)childClassName;
+    if ([childClassName isKindOfClass:[NSString class]]) {
+        typeStr = @"String";
+    }else if([childClassName isKindOfClass:[@(YES) class]]){
+        typeStr = @"Bool";
+    }else if([childClassName isKindOfClass:[NSNumber class]]){
+        NSString *valueStr = [NSString stringWithFormat:@"%@",childClassName];
+        if ([valueStr rangeOfString:@"."].location!=NSNotFound){
+            typeStr = @"Double";
+        }else{
+            typeStr = @"Int";
+        }
+    }else if([childClassName isKindOfClass:[NSNull class]]) {
+        typeStr = @"String";
     }
-    NSString *tmpStr = [NSString stringWithFormat:@"\n        %@ json <| \"%@\"",indexStr ,key];
-    if (nextFlag == 0) {
-        if (self.optional == YES) {
-            tmpStr = [NSString stringWithFormat:@"\n        %@ json <|? \"%@\"",indexStr ,key];
-        }
-    } else if (nextFlag == 1) {
-        tmpStr = [NSString stringWithFormat:@"\n        %@ json <| \"%@\"",indexStr ,key];
-    } else if(nextFlag == 2) {
-        if (self.optionalArray == YES) {
-            tmpStr = [NSString stringWithFormat:@"\n        %@ json <||? [\"%@\"]",indexStr ,key];
-        } else {
-            tmpStr = [NSString stringWithFormat:@"\n        %@ json <|| [\"%@\"]",indexStr ,key];
-        }
+
+//    NSString *indexStr = @"let json = JSON(json)";
+    NSString *tmpStr = [NSString stringWithFormat:@"\n    \tlet %@ = json[\"%@\"].%@" ,key,key,typeStr.lowercaseString];
+//    if (nextFlag == 0) {
+////        if (self.optional == YES) {
+////            tmpStr = [NSString stringWithFormat:@"\n        %@ json <|? \"%@\"",indexStr ,key];
+////        }
+//        return tmpStr
+//    } else
+    if (nextFlag == 1) {
+        LCStructInfo *tmpStructInfo = (LCStructInfo *)structInfo.infoDic[key];
+        NSString *childClassName = tmpStructInfo.structName;
         
+        tmpStr = [NSString stringWithFormat:@"\n\n    \tvar %@: %@? = nil \n    \tif let obj = json[\"%@\"].dictionaryObject { \n    \t\t%@ = (obj as NSDictinoary).mapToObject(%@.self) \n    \t}" ,key,childClassName, key , key, childClassName];
+    } else if(nextFlag == 2) {
+        
+        LCStructInfo *tmpStructInfo = (LCStructInfo *)structInfo.infoDic[key];
+        NSString *childClassName = tmpStructInfo.structName;
+        
+        tmpStr = [NSString stringWithFormat:@"\n\n    \tvar %@: %@? = nil \n    \tif let obj = json[\"%@\"].array { \n     \t\t%@ = (obj as NSArray).mapToObjectArray(%@.self) \n    \t}" ,key,childClassName, key , key, childClassName];
         
     }
     return tmpStr;
+    
+    
 }
+
+//- (NSString *)formatDetailWithKey:(NSString *)key next:(NSInteger)nextFlag{
+//
+//    
+//    
+//    NSString *indexStr = @"let json = JSON(json)";
+////    if (self.firstParseDetailContent == YES) {
+////        indexStr = @"<^>";
+////        self.firstParseDetailContent = NO;
+////    }
+//    NSString *tmpStr = [NSString stringWithFormat:@"%@\n   let %@ = json\[\"%@\"\]",indexStr ,key];
+//    if (nextFlag == 0) {
+//        if (self.optional == YES) {
+//            tmpStr = [NSString stringWithFormat:@"\n        %@ json <|? \"%@\"",indexStr ,key];
+//        }
+//    } else if (nextFlag == 1) {
+//        tmpStr = [NSString stringWithFormat:@"\n        %@ json <| \"%@\"",indexStr ,key];
+//    } else if(nextFlag == 2) {
+//        if (self.optionalArray == YES) {
+//            tmpStr = [NSString stringWithFormat:@"\n        %@ json <||? [\"%@\"]",indexStr ,key];
+//        } else {
+//            tmpStr = [NSString stringWithFormat:@"\n        %@ json <|| [\"%@\"]",indexStr ,key];
+//        }
+//        
+//        
+//    }
+//    return tmpStr;
+//}
 
 #pragma mark parse property
 - (NSString *)formatArrayWithKey:(NSString *)key value:(NSObject *)value {
     NSString *typeStr = @"";
     typeStr = (NSString *)value;
-    if (value == nil) {
-        typeStr = @"String";
-        self.optionalArray = YES;
-        return [NSString stringWithFormat:@"    let %@: [%@]?",key,typeStr];
-    }
-    return [NSString stringWithFormat:@"    let %@: [%@]",key,typeStr];
+    
+    return [NSString stringWithFormat:@"    var %@: [%@]?",key,typeStr];
+    
+//    if (value == nil) {
+//        typeStr = @"String";
+//        self.optionalArray = YES;
+//        return [NSString stringWithFormat:@"    let %@: [%@]?",key,typeStr];
+//    }
+//    return [NSString stringWithFormat:@"    let %@: [%@]",key,typeStr];
 }
 
 - (NSString *)formatDicWithKey:(NSString *)key value:(NSObject *)value {
     NSString *typeStr = @"";
     typeStr = (NSString *)value;
-    return [NSString stringWithFormat:@"    let %@: %@",key,typeStr];
+    return [NSString stringWithFormat:@"    var %@: %@?",key,typeStr];
 }
 
 - (NSString *)formatPropertyWithKey:(NSString *)key value:(NSObject *)value {
@@ -248,10 +353,10 @@ static LCJSONFormatter *sharedPlugin;
             typeStr = @"Int";
         }
     }else if([value isKindOfClass:[NSNull class]]) {
-        typeStr = @"String?";
+        typeStr = @"String";
         self.optional = YES;
     }
-    return [NSString stringWithFormat:@"    let %@: %@",key,typeStr];
+    return [NSString stringWithFormat:@"    var %@: %@?",key,typeStr];
 }
 
 
@@ -286,7 +391,7 @@ static LCJSONFormatter *sharedPlugin;
     if (templateString.length == 0) {
         templateString = @"";
     }
-    NSString *string = @"import UIKit\n\nimport Argo\nimport Curry\n\n";
+    NSString *string = @"import Foundation";
     templateString = [NSString stringWithFormat:@"%@%@", templateString, string];
     
     return [templateString copy];
@@ -335,6 +440,13 @@ static LCJSONFormatter *sharedPlugin;
 		_detailContentStr = [[NSString alloc] init];
 	}
 	return _detailContentStr;
+}
+
+- (NSString *)inFunctionStr {
+    if(_inFunctionStr == nil) {
+        _inFunctionStr = [[NSString alloc] init];
+    }
+    return _inFunctionStr;
 }
 
 - (NSString *)totalFuncStr {
